ded153cf196685ce97beba0c296ab76df1af2022
Nicole Macfarlane <nicole@craftypenguins.net>
2022-11-28 13:31:06 -0800

    Test middleware update

--- middleware.js
+++ middleware.js
@@ -32,7 +32,5 @@ export function middleware(request) {
 
   if (ip == '207.216.164.103') {
     return next({ headers: { 'x-ip-blocked': 'false'}, });
-  } else {
-    return next({ headers: { 'x-ip-blocked': 'true'}, });
   }
 }
