db40f2cbbe70b2889dc63504cf547b6fcdf34215
Nicole Macfarlane <nicole@craftypenguins.net>
2022-11-28 14:53:58 -0800

    Test middleware update

--- middleware.js
+++ middleware.js
@@ -1,13 +1,26 @@
 import { ipAddress, next } from '@vercel/edge';
+var ipRangeCheck = require("./ip-range-check");
 //import { BlockList } from 'net';
 
 export default function middleware(request) {
   const ip = ipAddress(request);
   const url = new URL(request.url);
-  const ipList = new BlockList;
+  //const ipList = new BlockList;
+  const ipWhitelist = ["199.83.128.0/21",
+                       "198.143.32.0/19",
+                       "149.126.72.0/21",
+                       "103.28.248.0/22",
+                       "45.64.64.0/22",
+                       "185.11.124.0/22",
+                       "192.230.64.0/18",
+                       "107.154.0.0/16",
+                       "45.60.0.0/16",
+                       "45.223.0.0/16",
+                       "2a02:e980::/29",
+                       "207.216.164.103"]
 
   //Imperva IPs
-  ipBlocklist.addSubnet('199.83.128.0', 21, 'ipv4');
+  /*ipBlocklist.addSubnet('199.83.128.0', 21, 'ipv4');
   ipBlocklist.addSubnet('198.143.32.0', 19, 'ipv4');
   ipBlocklist.addSubnet('149.126.72.0', 21, 'ipv4');
   ipBlocklist.addSubnet('103.28.248.0', 22, 'ipv4');
@@ -20,10 +33,11 @@ export default function middleware(request) {
   ipBlocklist.addSubnet('2a02:e980::', 29, 'ipv6');
 
   //Nicole testing
-  ipBlocklist.addAddress('207.216.164.103');
+  ipBlocklist.addAddress('207.216.164.103');*/
 
   //The blocklist contains our whitelisted IPs, so if not on blocklist then allow
-  if (!ipBlocklist.check(ip)) {
+//  if (!ipBlocklist.check(ip)) {
+  if (ipRangeCheck(ip, ipWhitelist)) {
     return next();
   } else {
     url.pathname = '/blocked.html';
